571d6bcc6c08a5a9143d27142f87fb3ac9cdd1b0
Stefan Arisona <robot@arisona.ch>
2018-05-11 18:17:54 +0200

    switch delay time mechanism / int32_t -> int

--- lib/objects/rmx/code/rmx_dsp_lib.hpp
+++ lib/objects/rmx/code/rmx_dsp_lib.hpp
@@ -2,31 +2,34 @@
 
 namespace rmx {
 
-static const int32_t BUFSIZEPOW = 4;
+static const int BUFSIZEPOW = 4;
 
-static const int32_t ONE = (1 << 27) - 1;
-static const int32_t MIN = -(1 << 27);
+static const int ONE = (1 << 27) - 1;
+static const int MIN = -(1 << 27);
 
 
-
-inline int32_t clamp(int32_t min, int32_t max, int32_t value) {
-   return std::max<int32_t>(min, std::min<int32_t>(max, value));
+inline int clamp(int min, int max, int value) {
+   return std::max(min, std::min(max, value));
 }
 
 inline float clamp(float min, float max, float value) {
-   return std::max<float>(min ,std::min<float>(max, value));
+   return std::max(min ,std::min(max, value));
 }
 
+inline int mod(int a, int b) {
+    int r = a % b;
+    return r < 0 ? r + b : r;
+}
 
 
 // EnvelopeFollower
 class EnvelopeFollower final {	
 public:
-    EnvelopeFollower(uint32_t attack = 0, uint32_t release = 0) {
+    EnvelopeFollower(int attack = 0, int release = 0) {
         update(attack, release);
     }
 
-    void update(int32_t attack, int32_t release) {
+    void update(int attack, int release) {
 		if (attack != this->attack) {
 			this->attack = attack;
 			a = time2gain(attack);
@@ -38,15 +41,15 @@ public:
 		}
     }
 
-	int32_t process(int32_t attack, int32_t release, const int32_t* input0, const int32_t* input1) {
+	int process(int attack, int release, const int32_t* input0, const int32_t* input1) {
         update(attack, release);
 		return process(input0, input1);
 	}
 
-	int32_t process(const int32_t* input0, const int32_t* input1) {
-		int32_t level;
+	int process(const int32_t* input0, const int32_t* input1) {
+		int level;
 		for (int i = 0; i < BUFSIZE; ++i) {
-			int32_t s;
+			int s;
 			s = input0[i] >> (BUFSIZEPOW + 1);
 			level += s > 0 ? s : -s;
 			s = input1[i] >> (BUFSIZEPOW + 1);
@@ -61,12 +64,12 @@ public:
 		return __SSAT(env, 28);
     }
 
-	inline int32_t get() const {
+	inline int get() const {
 		return env;
 	}
 
 private:
-	inline int32_t time2gain(int32_t t) {
+	static inline int time2gain(int t) {
         static const float EXP_T[64] = {
             0.001f, 0.001155f, 0.001334f, 0.00154f, 0.001778f, 0.002054f, 0.002371f, 0.002738f,
             0.003162f, 0.003652f, 0.004217f, 0.00487f, 0.005623f, 0.006494f, 0.007499f, 0.00866f, 
@@ -82,12 +85,12 @@ private:
 		return float_to_q27((float)pow(0.1, 1.0 / (EXP_T[t] * SAMPLERATE / BUFSIZE)));
 	}
 
-	int32_t attack = -1;
-	int32_t release = -1;
+	int attack = -1;
+	int release = -1;
 
-	int32_t a = 0;
-	int32_t r = 0;
-	int32_t env = 0;
+	int a = 0;
+	int r = 0;
+	int env = 0;
 };
 
 
@@ -96,7 +99,7 @@ public:
 	EnvelopeFollowerSimple() {
 	}
 
-	int32_t process(const int32_t* input0, const int32_t* input1) {
+	int process(const int32_t* input0, const int32_t* input1) {
 		int32_t level = 0;
 		for (int i = 0; i < BUFSIZE; ++i) {
 			int32_t s;
@@ -110,14 +113,14 @@ public:
 		return envelope;
 	}
 
-	inline int32_t get() const {
+	inline int get() const {
 		return envelope;
 	}
 
 private:
 	// 2=1.3ms 3=2.7ms 4=5.3ms 5=10.6ms 6=21.2ms 7=42.6ms 8=85.3ms 9=170.7ms
-	int32_t slope = 4;
-	int32_t envelope = 0;
+	int slope = 4;
+	int envelope = 0;
 };
 
 
@@ -127,8 +130,8 @@ class SVF final {
 public:
 	SVF() {}
 
-	void update(int32_t cutoff, uint32_t reso) {
-		int32_t alpha = 0;
+	void update(int cutoff, int reso) {
+		int alpha = 0;
 		MTOFEXTENDED(cutoff, alpha);
 		SINE2TINTERP(alpha, freq);		
 
@@ -136,39 +139,34 @@ public:
 		damp = ___SMMUL(damp, damp);
 	}
 
-	inline int32_t filterLow(int32_t sample) {
-		int32_t notch = sample - (___SMMUL(damp, band) << 1);
+	inline int filterLow(int sample) {
+		int notch = sample - (___SMMUL(damp, band) << 1);
 		low = low + (___SMMUL(freq, band) << 1);
-		int32_t high = notch - low;
+		int high = notch - low;
 		band = (___SMMUL(freq, high) << 1) + band;
 		return low;
 	}
 
-	inline int32_t filterHigh(int32_t sample) {
-		int32_t notch = sample - (___SMMUL(damp, band) << 1);
+	inline int filterHigh(int sample) {
+		int notch = sample - (___SMMUL(damp, band) << 1);
 		low = low + (___SMMUL(freq, band) << 1);
-		int32_t high = notch - low;
+		int high = notch - low;
 		band = (___SMMUL(freq, high) << 1) + band;
 		return high;
 	}
 
 private:
-	int32_t freq = 0;
-	int32_t damp = 0;
+	int freq = 0;
+	int damp = 0;
 
-	int32_t low = 0;
-	int32_t band = 0;
+	int low = 0;
+	int band = 0;
 };
 
 
 class FadeDelay final {
 public:
-    FadeDelay(int32_t lengthPow, int32_t fadeTime)  {
-        this->lengthPow = lengthPow;
-        length = 1 << lengthPow;
-        lengthMask = length - 1;
-
-        step = (1 << 27) >> fadeTime;
+    FadeDelay(int length, int fadeTime) : length(length), step((1 << 27) >> fadeTime)  {
     }
 
     void setup(int32_t* bufferL, int32_t* bufferR) {
@@ -182,11 +180,11 @@ public:
     }
 
     void update(const int32_t* inBufL, const int32_t* inBufR,
-                const int32_t time, const int32_t offset, const int32_t timeMod,
-                const int32_t feedback, const int32_t pingpong,
-                const int32_t hpCutoff, const int32_t hpReso, const int32_t hpMod,
-                const int32_t lpCutoff, const int32_t lpReso, const int32_t lpMod,
-                const int32_t modRate, const int32_t modMix) {
+                const int time, const int offset, const int timeMod,
+                const int feedback, const int pingpong,
+                const int hpCutoff, const int hpReso, const int hpMod,
+                const int lpCutoff, const int lpReso, const int lpMod,
+                const int modRate, const int modMix) {
 
         // update time / read offsets
         if (!isFading && (time != this->time || offset != this->offset)) {
@@ -195,9 +193,10 @@ public:
 
             // update read offset
             fadeOutOffsetL = fadeInOffsetL;
-            fadeInOffsetL = (time >> (27 - lengthPow)) - length;
+            fadeInOffsetL = time2offset(time, length);
+
             fadeOutOffsetR = fadeInOffsetR;
-            fadeInOffsetR = ((time + offset) >> (27 - lengthPow)) - length;
+            fadeInOffsetR = fadeInOffsetL + time2offset(offset, length);
             
             isFading = true;
             fadeInLevel = 0;
@@ -211,7 +210,7 @@ public:
 
         // update lfo modulation
         if (modRate > MIN) {
-            int32_t mod;
+            int mod;
             MTOFEXTENDED(modRate, mod)
             modPhase += mod >> 6;
         }
@@ -225,7 +224,7 @@ public:
         }
 
         if (modRate > MIN) {
-            int32_t mod;
+            int mod;
             SINE2TINTERP(modPhase, mod);	
             mod = ___SMMUL(mod, mod);
             this->hpMod = ___SMMUL(mod, hpMod);
@@ -236,14 +235,13 @@ public:
         }
 
         // update env modulation
-        int32_t env = envelope.process(inBufL, inBufR);
+        int env = envelope.process(inBufL, inBufR);
         this->timeMod = __SSAT(this->timeMod + ___SMMUL(env, ___SMMUL(timeMod, modMix) >> 8), 28);
         this->hpMod = __SSAT(this->hpMod + ___SMMUL(env << 3, ___SMMUL(hpMod << 3, modMix << 2) << 3), 28);
         this->lpMod = __SSAT(this->lpMod + ___SMMUL(env << 3, ___SMMUL(lpMod << 3, modMix << 2) << 3), 28);
-        debug = modMix;
 
         // update filters
-        int32_t cutoff;
+        int cutoff;
         cutoff = __SSAT(hpCutoff + this->hpMod, 28);
         hpL.update(cutoff, hpReso);
         hpR.update(cutoff, hpReso);
@@ -253,32 +251,32 @@ public:
         lpR.update(cutoff, lpReso);
     }
 
-    int32_t debug = 0;
+    int debug = 0;
 
     void process(const int32_t* inBufL, const int32_t* inBufR, int32_t* outBufL, int32_t* outBufR) {
         for (int s = 0; s < BUFSIZE; ++s) {
             // update feedback 
-            int32_t feedbackL = ___SMMUL(lastOutL << 3, sendLR << 2) + ___SMMUL(lastOutR << 3, sendRL << 2);	
-            int32_t feedbackR = ___SMMUL(lastOutR << 3, sendLR << 2) + ___SMMUL(lastOutL << 3, sendRL << 2);	
+            int feedbackL = ___SMMUL(lastOutL << 3, sendLR << 2) + ___SMMUL(lastOutR << 3, sendRL << 2);	
+            int feedbackR = ___SMMUL(lastOutR << 3, sendLR << 2) + ___SMMUL(lastOutL << 3, sendRL << 2);	
             feedbackL = ___SMMUL(feedbackL << 3, feedback << 2);
             feedbackR = ___SMMUL(feedbackR << 3, feedback << 2);
             
             // apply pingpong to input
-            int32_t inL = ___SMMUL(inBufL[s] << 3, sendLR << 2) + ___SMMUL(inBufL[s] << 3, sendRL << 2);	
-            int32_t inR = ___SMMUL(inBufR[s] << 3, sendLR << 2) + ___SMMUL(inBufR[s] << 3, sendRL << 2);	
+            int inL = ___SMMUL(inBufL[s] << 3, sendLR << 2) + ___SMMUL(inBufL[s] << 3, sendRL << 2);	
+            int inR = ___SMMUL(inBufR[s] << 3, sendLR << 2) + ___SMMUL(inBufR[s] << 3, sendRL << 2);	
 
             // update delay
-            writepos = (writepos + 1) & lengthMask;
+            writepos = mod((writepos + 1), length);
             bufferL[writepos] = __SSAT(inL + feedbackL, 28);
             bufferR[writepos] = __SSAT(inR + feedbackR, 28);
 
             // read and mix fade in / out
-            int indexInL = (writepos - fadeInOffsetL - timeMod) & lengthMask;
-            int indexInR = (writepos - fadeInOffsetR - timeMod) & lengthMask;
-            int indexOutL = (writepos - fadeOutOffsetL - timeMod) & lengthMask;
-            int indexOutR = (writepos - fadeOutOffsetR - timeMod) & lengthMask;
-            int32_t outL = ___SMMUL(bufferL[indexInL] << 3, fadeInLevel << 2) + ___SMMUL(bufferL[indexOutL] << 3, fadeOutLevel << 2);
-            int32_t outR = ___SMMUL(bufferR[indexInR] << 3, fadeInLevel << 2) + ___SMMUL(bufferR[indexOutR] << 3, fadeOutLevel << 2);
+            int indexInL = mod((writepos - fadeInOffsetL - timeMod), length);
+            int indexInR = mod((writepos - fadeInOffsetR - timeMod), length);
+            int indexOutL = mod((writepos - fadeOutOffsetL - timeMod), length);
+            int indexOutR = mod((writepos - fadeOutOffsetR - timeMod), length);
+            int outL = ___SMMUL(bufferL[indexInL] << 3, fadeInLevel << 2) + ___SMMUL(bufferL[indexOutL] << 3, fadeOutLevel << 2);
+            int outR = ___SMMUL(bufferR[indexInR] << 3, fadeInLevel << 2) + ___SMMUL(bufferR[indexOutR] << 3, fadeOutLevel << 2);
 
             // filter output
             outL = lpL.filterLow(hpL.filterHigh(outL));
@@ -301,43 +299,62 @@ public:
     }
 
 private:
+    inline int time2offset(int t, int length) {
+        // this table is pretty ad-hoc. it increases non-linear first, afterwards linear up to 2 seconds.
+        static const int OFFSETS[128] = {
+            10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 180, 220, 240,
+            1725, 3210, 4695, 6180, 7665, 9150, 10635, 12120, 13605, 15090, 16575, 18060, 19545, 21030, 22515, 24000,
+            
+            24750, 25500, 26250, 27000, 27750, 28500, 29250, 30000, 30750, 31500, 32250, 33000, 33750, 34500, 35250, 36000, 
+            36750, 37500, 38250, 39000, 39750, 40500, 41250, 42000, 42750, 43500, 44250, 45000, 45750, 46500, 47250, 48000,
+
+            48750, 49500, 50250, 51000, 51750, 52500, 53250, 54000, 54750, 55500, 56250, 57000, 57750, 58500, 59250, 60000,
+            60750, 61500, 62250, 63000, 63750, 64500, 65250, 66000, 66750, 67500, 68250, 69000, 69750, 70500, 71250, 72000,
+
+            72750, 73500, 74250, 75000, 75750, 76500, 77250, 78000, 78750, 79500, 80250, 81000, 81750, 82500, 83250, 84000,
+            84750, 85500, 86250, 87000, 87750, 88500, 89250, 90000, 90750, 91500, 92250, 93000, 93750, 94500, 95250, 96000,
+        };
+
+		t = (t >> (27 - 7)) & 0x7f;
+        return std::min(OFFSETS[t], length - 1);
+    }
+
+
     int32_t* bufferL = nullptr;
     int32_t* bufferR = nullptr;
 
-    uint32_t lengthPow = 0;
-    uint32_t length = 0;
-    uint32_t lengthMask = 0;
+    int length = 0;
 
-    uint32_t step = 0;
+    int step = 0;
 
-    uint32_t writepos = 0;
+    int writepos = 0;
 
-    uint32_t fadeInOffsetL = 0;
-    uint32_t fadeInOffsetR = 0;
-    int32_t fadeInLevel = 0;
+    int fadeInOffsetL = 0;
+    int fadeInOffsetR = 0;
+    int fadeInLevel = 0;
 
-    uint32_t fadeOutOffsetL = 0;
-    uint32_t fadeOutOffsetR = 0;
-    int32_t fadeOutLevel = ONE;
+    int fadeOutOffsetL = 0;
+    int fadeOutOffsetR = 0;
+    int fadeOutLevel = ONE;
 
     bool isFading = false;
 
     // delay time and offset (cached, so we can track when time changes to initiated fading)
-    int32_t time = 0;
-    int32_t offset = 0;
+    int time = 0;
+    int offset = 0;
 
     // feedback & pingpong
-    int32_t feedback = 0;
-    int32_t sendLR = 0;
-    int32_t sendRL = 0;
-    int32_t lastOutL = 0;
-    int32_t lastOutR = 0;
+    int feedback = 0;
+    int sendLR = 0;
+    int sendRL = 0;
+    int lastOutL = 0;
+    int lastOutR = 0;
 
     // modulation
     uint32_t modPhase = 0;
-    int32_t timeMod = 0;
-    int32_t hpMod = 0;
-    int32_t lpMod = 0;
+    int timeMod = 0;
+    int hpMod = 0;
+    int lpMod = 0;
     EnvelopeFollowerSimple envelope;
 
     // lp and hp filter states
